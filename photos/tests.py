1658dc57f24ce7ef80e55c82bbe03135e511d183
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-02 13:07:55 +0300

    Create test for the location model save method

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.nairobi=Location(name = 'Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+
+    def test_save_method(self):
+        self.nairobi.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)        
