a10943f8097abd48f5e970b9de6f44c1bdedb1be
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-02 12:15:45 +0300

    Create image model

--- photos/models.py
+++ photos/models.py
@@ -14,4 +14,11 @@ class Category(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.name          
+        return self.name
+
+class Image(models.Model):
+    image = models.CharField(max_length=30)
+    image_name = models.CharField(max_length=40)
+    image_description = models.TextField()
+    image_location = models.ForeignKey(Location)
+    image_category = models.ForeignKey(Category)                  
