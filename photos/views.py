4c41202a5445f21d190fd3888ba7e26a0329afe1
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-02 10:27:25 +0300

    Add location_taken view function

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 
 # Create your views here.
+def welcome(request):
+    return render(request,'welcome.html')
+def location_taken(request,place):
+#Do error handling
+#Filter the photos using the {place} param
+    pass
