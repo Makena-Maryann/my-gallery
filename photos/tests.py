adaedb5194ec284ac7c33537bf0463052f3b79de
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-02 13:20:05 +0300

    Test and create save method for location model

--- photos/tests.py
+++ photos/tests.py
@@ -11,5 +11,17 @@ class LocationTestClass(TestCase):
 
     def test_save_method(self):
         self.nairobi.save_location()
-        location = Location.objects.all()
-        self.assertTrue(len(location) > 0)        
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)  
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.food = Category(name = 'Food')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Category))
+
+    def test_save_method(self):
+        self.food.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)                      
