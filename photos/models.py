840646f310d5e1a6120a6361a7f3492b01031499
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-02 13:08:28 +0300

    Create save method for location model

--- photos/models.py
+++ photos/models.py
@@ -7,6 +7,9 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    def save_location(self):
+        self.save()    
+
     class Meta:
         ordering = ['name']  
 
