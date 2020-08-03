b46e2a3d144a8e1f23e4dd9fde9c4dcdf8d38f54
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-03 16:18:24 +0300

    Create tests for Image model methods get all images and images by location

--- photos/tests.py
+++ photos/tests.py
@@ -25,3 +25,28 @@ class CategoryTestClass(TestCase):
         self.food.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)                      
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.nairobi=Location(name='Nairobi')
+        self.nairobi.save_location()
+
+        self.food=Category(name='Food')
+        self.food.save_category()
+
+        self.new_image=Image(image_name='Banner',image_description='A test image',image_location=self.nairobi,image_category=self.food)
+        self.new_image.save()
+
+    def tearDown(self):  
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+        Image.objects.all().delete()  
+
+    def test_get_all_images(self):
+        all_images = Image.get_images()
+        self.assertTrue(len(all_images)>0)  
+
+    def test_filter_images_by_location(self):
+        test_location_id = 4
+        images_by_location = Image.filter_images_by_location(test_location_id)
+        self.assertTrue(len(images_by_location) == 0)      
