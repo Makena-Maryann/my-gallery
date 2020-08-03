e9c3790a4a9780e95e22873f3b93503a8ee92c8c
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-03 18:52:42 +0300

    Create view function to search for photos by category

--- photos/views.py
+++ photos/views.py
@@ -10,3 +10,16 @@ def all_images(request):
 def images_by_location_taken(request,location_id):
     photos = Image.filter_images_by_location(location_id)
     return render(request, 'photos/photo-location.html',{"photos":photos}) 
+
+def search_results(request):
+
+    if 'photo' in request.GET and request.GET["photo"]:
+        category = request.GET.get("photo")
+        searched_images = Image.search_image(category) 
+        message =f"{category}"
+
+        return render(request, 'photos/search.html',{"message":message, "photos": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'photos/search.html',{"message":message})    
