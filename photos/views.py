badd1e56060bd64c6711be24c1f843d410fe1f4c
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-03 16:16:08 +0300

    Create view functions to display all images and images based on the location taken

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
+from .models import Image
 
 # Create your views here.
 def all_images(request):
-    return render(request,'welcome.html')
-def images_by_location_taken(request,place):
-#Do error handling
-#Filter the photos using the {place} param
-    pass
+    photos = Image.get_images()
+    return render(request,'photos/all-photos.html', {"photos":photos})
+
+def images_by_location_taken(request,location_id):
+    photos = Image.filter_images_by_location(location_id)
+    return render(request, 'photos/photo-location.html',{"photos":photos}) 
