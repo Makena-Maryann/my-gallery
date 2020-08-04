372a33cbd98de75bf19b62a10f0afe94288a7844
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-04 13:56:27 +0300

    Get all locations and categories from the db

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
-from .models import Image
+from .models import Image,Location,Category
 
 # Create your views here.
 def all_images(request):
     photos = Image.get_images()
-    return render(request,'photos/all-photos.html', {"photos":photos})
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    return render(request,'photos/all-photos.html', {"photos":photos, "locations":locations, "categories":categories,})
 
 def images_by_location_taken(request,location_id):
     photos = Image.filter_images_by_location(location_id)
